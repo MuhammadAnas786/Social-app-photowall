60841ef5755e2773845f9273cad12d74de2782b0
anasikram <anasikram501@gmail.com>
2021-03-30 14:51:28 +0500

    Initial Commit

--- src/Database/config.js
+++ src/Database/config.js
@@ -1,12 +1,6 @@
 import  firebase from "firebase"
 const firebaseConfig = {
-    apiKey: "AIzaSyAUA1XO8kdWDHpDb0ineOpQStn6bvxUyHw",
-    authDomain: "photowall-5be56.firebaseapp.com",
-    databaseURL: "https://photowall-5be56-default-rtdb.firebaseio.com",
-    projectId: "photowall-5be56",
-    storageBucket: "photowall-5be56.appspot.com",
-    messagingSenderId: "751304939022",
-    appId: "1:751304939022:web:b692991a1342b721af4e5f"
+ //Add your firebase api information here..
   };
 
   firebase.initializeApp(firebaseConfig)
